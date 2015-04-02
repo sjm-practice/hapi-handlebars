e3c443192d501e4d3e4b8ea9c291b5b5297d3f41
stevenjmarsh <sjm.atl@gmail.com>
2015-04-02 13:36:41 -0700

    view index from root route

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # hapi-handlebars
 A simple node hapi server for practice with dynamic handlebars layout and partial templates.
+
+## Items learned, practiced...
+* trying a new directory structure and entry point
+    - using bin/www pattern (as used by Codeschool and reportedly, Node personnel)
+    - trying a wwwd entry point for debug (node-debug)
+        + MAY ABANDON THIS APPROACH/EXPIREMENT, IF NO BENEFITS FOUND
+
+## Notes
+* ...
+
+## Tutorial / Resources Feedback
+* ... 
---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var Hapi = require('hapi');
+
+/** 
+ * Create and setup new Hapi server.
+ */
+var server = new Hapi.Server();
+server.connection({
+  port: 3000
+});
+
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: 'views',
+  layoutPath: 'views/layout',
+  layout: 'default',
+  helpersPath: 'views/helpers'
+  //partialsPath: 'views/partials'
+});
+
+/** 
+ * Set up server routes.
+ */
+server.route({
+  path: '/',
+  method: 'GET',
+  handler: function (request, reply) {
+    var templateData = {
+      title: 'This is Index!',
+      message: 'Hello, World! via handlebars template'
+    };
+
+    return reply.view('index', templateData);
+  }
+});
+
+module.exports = server;
